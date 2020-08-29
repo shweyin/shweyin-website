0f50866a796818b1fee7ca88d695010404ea0880
shweyin <shweyin@gmail.com>
2020-08-29 13:18:28 -0400

    added links

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,24 +1,40 @@
 <template>
     <v-navigation-drawer app dark class="grey darken-4">
         <v-list>
-            <v-list-item class="pa-5">
+            <v-list-item class="pa-3">
                 <v-img src="@/assets/shweyin-shot.jpg" class="rounded"></v-img>
             </v-list-item>
             <v-list-item link to="/">
-            <v-list-item-action>
-                <v-icon>mdi-view-dashboard</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-                <v-list-item-title>Home</v-list-item-title>
-            </v-list-item-content>
+                <v-list-item-action>
+                    <v-icon>mdi-view-dashboard</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>Home</v-list-item-title>
+                </v-list-item-content>
             </v-list-item>
-            <v-list-item link to="/about">
-            <v-list-item-action>
-                <v-icon>mdi-history</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-                <v-list-item-title>History</v-list-item-title>
-            </v-list-item-content>
+            <v-list-item link to="/profile">
+                <v-list-item-action>
+                    <v-icon>mdi-account-box</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>Profile</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item link to="/projects">
+                <v-list-item-action>
+                    <v-icon>mdi-code-tags</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>Projects</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item link to="/history">
+                <v-list-item-action>
+                    <v-icon>mdi-history</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>History</v-list-item-title>
+                </v-list-item-content>
             </v-list-item>
         </v-list>
     </v-navigation-drawer>
